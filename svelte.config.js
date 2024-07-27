c1443ee0cf54d1a9be819d8c7ad9c3c35e79e186
badoge <18620902+badoge@users.noreply.github.com>
2024-07-28 01:13:43 +0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,8 @@ import adapter from '@sveltejs/adapter-cloudflare';
 export default {
 	kit: {
 		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
+			include: ['/*'],
+			exclude: ['<all>']
 		})
 	}
 };

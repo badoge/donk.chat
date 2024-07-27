1c90bd864f5e28babe0e35a77b024d1afc8d9175
badoge <18620902+badoge@users.noreply.github.com>
2024-07-28 01:15:06 +0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter({
-			include: ['/*'],
-			exclude: ['<all>']
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
+
+export default config;
